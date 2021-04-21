8c09343cf8da46fd67ab7429ef76426a9d31d95c
Gabriel <47752724+Botche@users.noreply.github.com>
2021-04-21 19:12:02 +0300

    Add toastr for notifications.

---
+++ src/TaskScript.Application/TaskScript.Application/Constants/NotificationsConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskScript.Application.Constants
+{
+    public static class NotificationsConstants
+    {
+        public const string SuccessNotification = "Success";
+        public const string ErrorNotification = "Error";
+        public const string WarningNotification = "Warning";
+        public const string InfoNotification = "Info";
+
+        public const string SuccessfullyAddedRoles = "Successfully added roles!";
+        public const string AlreadyCreatedRoles = "Default roles are already created.";
+    }
+}
