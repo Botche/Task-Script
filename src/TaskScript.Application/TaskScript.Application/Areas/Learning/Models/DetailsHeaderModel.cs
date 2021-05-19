dbe5aaab4229c9c68c157bb9e2f60850186a202e
Gabriel <47752724+Botche@users.noreply.github.com>
2021-05-19 17:37:24 +0300

    Fix details header partial view

---
+++ src/TaskScript.Application/TaskScript.Application/Areas/Learning/Models/DetailsHeaderModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskScript.Application.Areas.Learning.Models
+{
+    public class DetailsHeaderModel
+    {
+        public int ObjectId { get; set; }
+
+        public string H1Text { get; set; }
+    }
+}
