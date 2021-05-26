ad16b847c860e2eb2fe8a1787d67504011ed2094
Gabriel <47752724+Botche@users.noreply.github.com>
2021-05-26 17:10:22 +0300

    Add red star for required fields with js

---
+++ src/TaskScript.Application/TaskScript.Application/wwwroot/js/requiredStar.js
@@ -0,0 +1,12 @@
+﻿(() => {
+    const requiredInputs = document.querySelectorAll('input[data-val-required]:not(input[type="hidden"])');
+
+    requiredInputs.forEach(requiredInput => {
+        const parent = requiredInput.parentNode;
+        const label = parent.querySelector('label');
+
+        if (label) {
+            label.innerHTML += '<span class="text-danger">*</span>';
+        }
+    });
+})();
